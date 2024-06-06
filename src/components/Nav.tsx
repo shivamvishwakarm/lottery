da65aafe232f0d3c305222774470178446bb75ed
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2024-06-06 11:03:13 +0530

    bug: Solve nav bar responsive issue

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { MenuOutlined } from '@ant-design/icons';
 
 interface NavProps {
     // Define your component props here
 }
 
 const Nav: React.FC<NavProps> = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav className='flex justify-around border-t-4 border-blue-700 items-center shadow-lg'>
-            <div className='flex items-center justify-center bg-blend-multiple'>
-                <Image className="p-2" src='/logo.jpg' alt='logo' width={300} height={300} />
+        <nav className='flex flex-col sm:flex-row justify-between items-center p-4 border-t-4 border-blue-700 shadow-lg'>
+            <div className='flex items-center justify-between w-full sm:w-auto'>
+                <Image className="p-2" src='/logo.jpg' alt='logo' width={80} height={80} />
+                <button 
+                    className='sm:hidden p-2'
+                    onClick={toggleMenu}
+                >
+                    <MenuOutlined className='text-2xl' />
+                </button>
             </div>
-            <div className='flex'>
-                <ul className='flex'>
-                    <li className='mx-2'>Home</li>
-                    <li className='mx-2'>Account Details</li>
-                    <li className='mx-2'>Contact</li>
+            <div className={`flex-col sm:flex-row flex ${isOpen ? 'flex' : 'hidden'} sm:flex w-full sm:w-auto mt-4 sm:mt-0`}>
+                <ul className='flex flex-col sm:flex-row w-full sm:w-auto'>
+                    <li className='mx-2 my-1 sm:my-0'>Home</li>
+                    <li className='mx-2 my-1 sm:my-0'>Account Details</li>
+                    <li className='mx-2 my-1 sm:my-0'>Contact</li>
                 </ul>
             </div>
-            <div className=''>
+            <div className='mt-4 sm:mt-0'>
                 <p>📞 +91 7894561230</p>
             </div>
         </nav>
